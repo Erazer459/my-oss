0b784cbc057ee29b746cac8ed10f1e8a654870cd
FranzLi347 <ingachi@163.com>
2022-12-15 11:22:40 +0800

    feat:add files and bucket

---
+++ src/main/java/io/github/franzli347/foss/mapper/BucketMapper.java
@@ -0,0 +1,18 @@
+package io.github.franzli347.foss.mapper;
+
+import io.github.franzli347.foss.entity.Bucket;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author FranzLi
+* @description 针对表【tb_bucket】的数据库操作Mapper
+* @createDate 2022-12-15 10:46:45
+* @Entity io.github.franzli347.foss.entity.Bucket
+*/
+public interface BucketMapper extends BaseMapper<Bucket> {
+
+}
+
+
+
+
