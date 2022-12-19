fe19eea755120dc1c8205dfe2214ad7adc050480
franzli <92065397+FranzLi347@users.noreply.github.com>
2022-12-19 20:02:02 +0800

    Revert "feat:add FileZipUtil and compress file"

--- f-oss-master/src/main/java/io/github/franzli347/foss/support/fileSupport/ChunkPathResolver.java
+++
@@ -1,7 +0,0 @@
-package io.github.franzli347.foss.support.fileSupport;
-
-import java.util.List;
-
-public interface ChunkPathResolver {
-    List<String> getChunkPaths(String id, int chunks);
-}
--- f-oss-master/src/main/java/io/github/franzli347/foss/support/fileSupport/FileUploadPostProcessor.java
+++
@@ -1,7 +0,0 @@
-package io.github.franzli347.foss.support.fileSupport;
-
-import io.github.franzli347.foss.common.FileUploadParam;
-
-public interface FileUploadPostProcessor {
-    boolean process(String filePath, FileUploadParam param);
-}
