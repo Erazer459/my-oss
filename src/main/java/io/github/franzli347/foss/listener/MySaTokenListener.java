2f2ed13c0ca9f63281b1ab0f3fac60c19150ed85
XC_chen <Erazer459>
2023-03-22 17:01:13 +0800

    chore:use tencent's ip api

--- src/main/java/io/github/franzli347/foss/listener/MySaTokenListener.java
+++ src/main/java/io/github/franzli347/foss/listener/MySaTokenListener.java
@@ -1,29 +1,19 @@
 package io.github.franzli347.foss.listener;
 
-/**
- * @ClassName MySaTokenListener
- * @Author AlanC
- * @Date 2023/1/9 17:12
- **/
 
 import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.listener.SaTokenListener;
 import cn.dev33.satoken.stp.SaLoginModel;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.bitwalker.useragentutils.UserAgent;
 import io.github.franzli347.foss.vo.LoginRecord;
 import io.github.franzli347.foss.service.UserService;
-import io.github.franzli347.foss.support.userSupport.LoginUserProvider;
 import io.github.franzli347.foss.utils.IPUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 /**
  * 自定义侦听器的实现
@@ -31,15 +21,11 @@ import java.util.Optional;
 @Component
 @Slf4j
 public class MySaTokenListener implements SaTokenListener {
-    private final LoginUserProvider loginUserProvider;
     private final UserService service;
     private final RestTemplate restTemplate;
-    private ObjectMapper objectMapper;
-    public MySaTokenListener(LoginUserProvider loginUserProvider, UserService service,RestTemplate restTemplate, ObjectMapper objectMapper) {
-        this.loginUserProvider = loginUserProvider;
+    public MySaTokenListener(UserService service,RestTemplate restTemplate) {
         this.service = service;
         this.restTemplate = restTemplate;
-        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -51,7 +37,7 @@ public class MySaTokenListener implements SaTokenListener {
                .time(LocalDateTime.now())
                .uid((Integer) loginId)
                .device(UserAgent.parseUserAgentString(request.getHeader("User-Agent")).getOperatingSystem().getName())
-               .city(IPUtil.getCityByIp(restTemplate,objectMapper,ip).getLocationInfo())
+               .city(IPUtil.getCityByIp(restTemplate,ip))
                .build());
 
     }
